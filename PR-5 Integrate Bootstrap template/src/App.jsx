fdc0d0d9562392f285f36d7a3f645246f79db3f4
Nisha-parmar01 <nishaparmar.0901@gmail.com>
2026-02-13 17:04:59 +0530

    PR-5

---
+++ PR-5 Integrate Bootstrap template/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import './App.css'
+import Footer from './components/fotter';
+import Faq from './pages/FAQ';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Faq />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
